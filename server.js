f015659a784452c654f28e647a2dbe5402d7a0f4
Daniel Ioni <rossieugenio17@gmail.com>
2026-08-01 02:34:05 +0200

    Aggiunto server Express funzionante per il deploy

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log('MyZubster Gateway is starting...');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('MyZubster Gateway is running!');
+});
+
+// Avvia il server
+app.listen(port, () => {
+  console.log(`MyZubster Gateway listening on port ${port}`);
+});
