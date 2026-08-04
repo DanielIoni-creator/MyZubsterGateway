c5f0cd936adccdda52ea04bb657a8cc61664b366
foxxx009 <309196184+foxxx009@users.noreply.github.com>
2026-08-05 03:03:44 +0800

    Mount escrow gateway routes (Issue #63)

--- server.js
+++ server.js
@@ -38,6 +38,7 @@ app.use('/api/rewards', require('./routes/rewards'));
 app.use('/api/bounty', require('./routes/bounty'));
 app.use('/api/stake', require('./routes/stake'));
 app.use('/api/escrow/house', require('./routes/escrowHouse'));
+app.use('/api/escrow', require('./routes/escrowGateway'));
 
 console.log('✅ Caricamento routes robot...');
 app.use('/api/robot', require('./routes/robot'));
