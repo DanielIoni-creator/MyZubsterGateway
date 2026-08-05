4dcb1274f44e0d5a1af3cfbd63d2257064bc5610
foxxx009 <309196184+foxxx009@users.noreply.github.com>
2026-08-06 02:14:20 +0800

    feat(ui): wire ErrorPage routes + ThemeProvider (#149)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Bounty from './pages/Bounty';
+import { ThemeProvider } from './contexts/ThemeContext';
+import ErrorBoundary from './components/ErrorBoundary';
 import Home from './pages/Home';
+import Bounty from './pages/Bounty';
+import ErrorPage from './pages/ErrorPage';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/bounty" element={<Bounty />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider>
+      <BrowserRouter>
+        <ErrorBoundary>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/bounty" element={<Bounty />} />
+            <Route path="/404" element={<ErrorPage type="404" />} />
+            <Route path="/500" element={<ErrorPage type="500" />} />
+            <Route path="/403" element={<ErrorPage type="403" />} />
+            <Route path="/rate-limit" element={<ErrorPage type="rate" />} />
+            <Route path="/maintenance" element={<ErrorPage type="maintenance" />} />
+            <Route path="/offline" element={<ErrorPage type="offline" />} />
+            <Route path="*" element={<ErrorPage type="404" />} />
+          </Routes>
+        </ErrorBoundary>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
