1332d0a0f28f82d668c1adf5414f5a0dcbac2096
waterWang <waterWang@users.noreply.github.com>
2026-08-08 08:37:11 +0800

    feat: add BENZINA-XMR station app with XMR earnings dashboard, station registration, transaction history, and exchange rate ticker (Closes #707) [MYZ]

--- server.js
+++ server.js
@@ -107,6 +107,10 @@ app.get('/hospital', (req, res) => {
   res.sendFile(path.join(__dirname, 'frontend/dist/hospital.html'));
 });
 
+app.get('/benzina-xmr', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend/dist/benzina-xmr.html'));
+});
+
 // Static frontend
 const frontendPath = path.join(__dirname, 'frontend/dist');
 app.use(express.static(frontendPath));
